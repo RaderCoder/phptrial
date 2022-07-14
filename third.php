cd89a2ba631ed370fe82bf57dffb87b931703821
Radee Al Mahmood <radee.mahmood@northsouth.edu>
2022-07-14 15:24:16 +0600

    Added The Third File

---
+++ third.php
@@ -0,0 +1,28 @@
+<?php
+/*
+$arr = [1,2,3,4];
+print_r($arr);
+$even = array(0,2,4,6);
+print '<br>';
+print_r($even);
+print '<br>';
+print 'The second element in even is '.$even[1].'<br>';
+
+$age = [
+  "Radee" => 24,
+  "Adib" => 23,
+  "Anjum" =>32
+];
+
+print_r($age);
+*/
+$coordinates = [
+  [12,34],
+  [24,48],
+  [56,78]
+];
+
+print '<br>';
+print_r($coordinates);
+
+?>
